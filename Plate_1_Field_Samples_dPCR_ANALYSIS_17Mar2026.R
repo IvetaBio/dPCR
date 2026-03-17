e1d6a47b3af1b567f2a22bb9211038f26d8e5712
Iveta <ivetacasey@gmail.com>
2026-03-17 13:28:59 -0500

    I removed an unwanted object

--- Plate_1_Field_Samples_dPCR_ANALYSIS_17Mar2026.R
+++ Plate_1_Field_Samples_dPCR_ANALYSIS_17Mar2026.R
@@ -3,4 +3,13 @@
 csv_path <- "R:/AES_MicrobiologicalArchive/Data/Geddes Lab/02_Geddes_Grad_Students/Iveta_Casey/Electronic Lab Notebook/Digital PCR/HWMC_Field_Samples_dPCR_Results/Plate_1_field_samples_dpcr_run_04Mar2026_analysis_17_03_2026_15_20_38_UTC+00_00.csv"
 
 #Reading in the data file 
-Plate_1_dPCR_data <- read.csv(csv_path, stringsAsFactors = FALSE)
+Plate_1_dPCR_data <- read.csv(csv_path, header = TRUE, stringsAsFactors = FALSE)
+
+#having issues reading in the csv file, so I am going to see how many fields there are 
+# asking if every row in this CSV file have the same number of comma-separated fields...
+field_counts <- count.fields(csv_path, sep = ",")
+table(field_counts)
+
+# results are showing that 193 rows have 25 columns...and there is one row that has 2 columns
+
+# trying to find the exact row that is giving me issues 
