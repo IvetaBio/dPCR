a3f43a7fbf849446a869725bd3458d3571ed1872
Iveta <ivetacasey@gmail.com>
2026-03-17 13:06:13 -0500

    trying to read in the CSV raw data file but there seems to be an issue with some rows having more columns than column names..

--- Plate_1_Field_Samples_dPCR_ANALYSIS_17Mar2026.R
+++ Plate_1_Field_Samples_dPCR_ANALYSIS_17Mar2026.R
@@ -1,3 +1,6 @@
 
 #copying path to file
 csv_path <- "R:/AES_MicrobiologicalArchive/Data/Geddes Lab/02_Geddes_Grad_Students/Iveta_Casey/Electronic Lab Notebook/Digital PCR/HWMC_Field_Samples_dPCR_Results/Plate_1_field_samples_dpcr_run_04Mar2026_analysis_17_03_2026_15_20_38_UTC+00_00.csv"
+
+#Reading in the data file 
+Plate_1_dPCR_data <- read.csv(csv_path, stringsAsFactors = FALSE)
