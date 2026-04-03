81c409696056c40f5324ddb32be70c5ee949fa90
Iveta <ivetacasey@gmail.com>
2026-04-03 09:16:35 -0500

    removing the original "read.csv" code because it doesn't work with the type of csv file from the QIAcuity comes from

--- Plate_1_Field_Samples_dPCR_ANALYSIS_17Mar2026.R
+++ Plate_1_Field_Samples_dPCR_ANALYSIS_17Mar2026.R
@@ -2,10 +2,9 @@
 #copying path to file
 csv_path <- "R:/AES_MicrobiologicalArchive/Data/Geddes Lab/02_Geddes_Grad_Students/Iveta_Casey/Electronic Lab Notebook/Digital PCR/HWMC_Field_Samples_dPCR_Results/Plate_1_field_samples_dpcr_run_04Mar2026_analysis_17_03_2026_15_20_38_UTC+00_00.csv"
 
+Plate_2_csv_path <- "R:/AES_MicrobiologicalArchive/Data/Geddes Lab/02_Geddes_Grad_Students/Iveta_Casey/Electronic Lab Notebook/Digital PCR/HWMC_Field_Samples_dPCR_Results/Plate_2_Field_Samples_dPCR_run_06Mar2026_IH_analysis_03_04_2026_14_12_40_UTC+00_00.csv"
+
 #Reading in the data file 
-Plate_1_dPCR_data <- read.csv(csv_path, header = TRUE, stringsAsFactors = FALSE)
-
-
 # Okay so the issue is with the first line.. so I am going to skip line 1
 Plate_1_dPCR_data <- read.csv(
   csv_path,
